82db5c54c92506a9123fb9920ab926f2ac4050f1
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-26 12:45:50 +0530

    Display second table and its value is depend on first table selected value

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -55,5 +55,42 @@ namespace WPF_College_Manager
             }
            
         }
+
+
+        private void ShowColleges()
+        {
+            try
+            {
+                string query = "SELECT * from College c inner join DistrictCollege dc on c.Id = dc.CollegeId where dc.DistrictId = @districtID";
+
+                SqlCommand sqlCommand = new SqlCommand(query,sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+
+                    //get the selected district and fit to query
+                    sqlCommand.Parameters.AddWithValue("@districtID",ListDistrict.SelectedValue);
+
+                    DataTable collegeTable = new DataTable();
+                    sqlDataAdapter.Fill(collegeTable);
+
+                    ListCollege.DisplayMemberPath = "CollegeName"; //sane as Column name
+                    ListCollege.SelectedValuePath = "Id";
+                    ListCollege.ItemsSource = collegeTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+
+        private void ListDistrict_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowColleges();
+
+        }
     }
 }
