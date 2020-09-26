8eb7cbf91a5443528ed2d12eeb5b78ab22b4ebf1
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-26 10:36:58 +0530

    Add project files.

---
+++ DBDemoDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace WPF_College_Manager
+{
+
+
+    partial class DBDemoDataSet
+    {
+    }
+}
+
+namespace WPF_College_Manager.DBDemoDataSetTableAdapters {
+    
+    
+    public partial class DistrictTableAdapter {
+    }
+}
